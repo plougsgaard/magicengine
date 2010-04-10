89d9356343b40aa86276590dbab567958943d346
Thomas Plougsgaard <plougsgaard@users.noreply.github.com>
2010-04-10 15:40:18 +0000

    Removed unused view and updated the other views to reflect the change.

--- sql/tables/cards.sql
+++ sql/tables/cards.sql
@@ -7,12 +7,14 @@ CREATE TABLE  `magicengine`.`cards` (
   `types` varchar(255) NOT NULL,
   `card_text` mediumtext NOT NULL,
   `expansion` varchar(255) NOT NULL,
-  `rarity` varchar(255) NOT NULL,
+  `set_code` varchar(5) DEFAULT NULL,
   `card_number` varchar(255) NOT NULL,
+  `rarity` varchar(255) NOT NULL,
   `artist` varchar(255) NOT NULL,
   `image` mediumblob NOT NULL,
   `price` double NOT NULL DEFAULT '0',
   `cutout` mediumblob,
+  `thumbnail` mediumblob,
   PRIMARY KEY (`id`),
   UNIQUE KEY `card_name` (`card_name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
--- sql/views/view_create_all.sql
+++ sql/views/view_create_all.sql
@@ -25,7 +25,7 @@ CREATE VIEW `magicengine`.`view_latestprices` AS
 
 SELECT
 price.id, price.card_id, price.seller_id, price.date_added, price.price
-FROM view_prices price
+FROM prices price
 JOIN view_maxprices AS most_recent_price
 ON
 price.date_added = most_recent_price.most_recent
