fccf63631a6fed7006a796ab0f9ac38d659ea762
thomasp <thomasp@18aef096-2a25-41ee-9674-4334d193c836>
2010-04-10 15:41:09 +0000

    Now also removed from the compound script.

--- sql/views/view_create_all.sql
+++ sql/views/view_create_all.sql
@@ -1,13 +1,4 @@
 /*
- * `magicengine`.`view_prices`
- */
-
-CREATE VIEW `magicengine`.`view_prices` AS
-
-SELECT id, card_id, seller_id, date_added, price
-FROM prices;
-
-/*
  * `magicengine`.`view_maxprices`
  */
 
