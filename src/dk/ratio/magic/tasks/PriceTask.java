6d38ab315f77ba8f7b599159704fe65aeec5d6d5
Thomas Plougsgaard <plougsgaard@users.noreply.github.com>
2010-04-18 16:22:08 +0000

    Fix.
    Price task now as Component. Seems to work better, but have to test on linux/mac first.

--- src/dk/ratio/magic/tasks/PriceTask.java
+++ src/dk/ratio/magic/tasks/PriceTask.java
@@ -7,12 +7,13 @@ import dk.ratio.magic.services.config.ConfigBean;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import java.security.SecureRandom;
 import java.util.List;
 
-@Service
+@Component
 public class PriceTask
 {
     protected final Log logger = LogFactory.getLog(getClass());
@@ -31,18 +32,11 @@ public class PriceTask
 
     public void update() throws InterruptedException
     {
-        randomSleep(60);
         if (configBean.isPriceTaskEnabled()) {
             List<QueueItem> firstInQueue = cardDao.getFirstInQueue();
             crawler.updatePrice(firstInQueue);
         } else {
-            logger.info("Price Task disabled.");
+            logger.info("(Tick!) -- price task disabled");
         }
-    }
-
-    private void randomSleep(int seconds) throws InterruptedException
-    {
-        int randomDelay = Math.abs(secureRandom.nextInt(seconds) * 1000);
-        Thread.sleep(randomDelay);
     }
 }
